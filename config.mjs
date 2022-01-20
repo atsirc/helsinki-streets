bd03a1a539fe419f70f58acf1435e2566644d385
atsirc <christa.hannuksela@gmail.com>
2022-01-20 21:57:56 +0200

    added config

---
+++ config.mjs
@@ -0,0 +1,7 @@
+export default {
+  user: 'christanicole',
+  key: 'eE4ilIHYiJwOCYt4R1Pp',
+  style: '353503b7-999d-4a43-a921-ba3e28c0dce3',
+  mb_token: 'pk.eyJ1IjoiY2hyaXN0YW5pY29sZSIsImEiOiJja3Ztc3ZoMmkxaXpsMm50a20xaWRvaGx3In0.VKuppjU2lFkkBW8xPL9iQQ'
+};
+
